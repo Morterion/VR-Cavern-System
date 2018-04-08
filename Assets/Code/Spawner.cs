367ac16e501aa728db0f861fa1840a189b0622f6
Juho Kuusinen <qurred@gmail.com>
2018-04-08 11:54:53 +0300

    Added some prefabs, lights and respawn

---
+++ Assets/Code/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Rigidbody Spawnable;
+    private Transform loc;
+    private Rigidbody newest;
+    public float range = 3f;
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position, range);
+    }
+
+    private void Start(){
+        loc = this.transform.transform;
+        newest = Instantiate(Spawnable, loc.position, loc.rotation) as Rigidbody;
+        newest.transform.SetParent(this.gameObject.transform);
+    }
+
+    private void Update()
+    {
+        float dist = (newest.position - loc.position).sqrMagnitude;
+        if(dist > range)
+        {
+            // We should also play sound
+            newest = Instantiate(Spawnable, loc.position, loc.rotation) as Rigidbody;
+            newest.transform.SetParent(this.gameObject.transform);
+        }
+    }
+}
