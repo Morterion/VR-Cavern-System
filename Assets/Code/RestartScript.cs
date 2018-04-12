0457b78eecc9fae044cc39ccd291d18805c2dbe4
Kasimir Piispa <kasimir.piispa@luukku.com>
2018-04-12 15:43:01 +0300

    Final version

---
+++ Assets/Code/RestartScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScript : MonoBehaviour {
+
+    public string scene;
+
+    void Start()
+    {
+        Debug.Log("Press R to restart");
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(scene);
+        }
+
+    }
+}
